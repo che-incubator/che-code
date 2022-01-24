1d26af598c6ccf7877e0095c7dd749bc5cc79013
Florent Benoit <fbenoit@redhat.com>
2022-01-24 16:16:31 +0100

    chore: use full path when importing modules

--- code/src/vs/server/node/webClientServer.ts
+++ code/src/vs/server/node/webClientServer.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { getCheRedirectLocation } from './che/webClientServer';
+import { getCheRedirectLocation } from 'vs/server/che/webClientServer';
 import * as fs from 'fs';
 import * as http from 'http';
 import * as url from 'url';
