396fba65f4f850721017d32a8f85a5c7f64ec85f
Artem Zatsarynnyi <azatsary@redhat.com>
2023-06-30 11:35:03 +0200

    Add a new auto-rebase replacement rule for the 'extensions/mangle-loader.js'
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>

---
+++ .rebase/replace/code/extensions/mangle-loader.js
@@ -0,0 +1,6 @@
+[
+    {
+        "from": "const ts2tsMangler = new Mangler(projectPath, log, { mangleExports: true, manglePrivateFields: true });",
+        "by": "const ts2tsMangler = new Mangler(projectPath, log, { mangleExports: false, manglePrivateFields: true });"
+    }
+]
