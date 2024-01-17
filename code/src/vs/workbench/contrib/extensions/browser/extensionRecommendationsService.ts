d437c7696f8121f2016dedef33e939f8c1b0701b
vitaliy-guliy <vgulyy@redhat.com>
2024-01-17 12:53:47 +0000

    fix: add delay before autoinstalling the recommendations
    Signed-off-by: vitaliy-guliy <vgulyy@redhat.com>

--- code/src/vs/workbench/contrib/extensions/browser/extensionRecommendationsService.ts
+++ code/src/vs/workbench/contrib/extensions/browser/extensionRecommendationsService.ts
@@ -113,16 +113,11 @@ export class ExtensionRecommendationsService extends Disposable implements IExte
 			this.remoteRecommendations.activate()
 		]);
 
-		// this._register(Event.any(
-		// 	this.workspaceRecommendations.onDidChangeRecommendations, 
-		// 	this.configBasedRecommendations.onDidChangeRecommendations, 
-		// 	this.extensionRecommendationsManagementService.onDidChangeIgnoredRecommendations)(() => this._onDidChangeRecommendations.fire()));
-
 		this._register(Event.any(
 			this.workspaceRecommendations.onDidChangeRecommendations, 
 			this.configBasedRecommendations.onDidChangeRecommendations, 
-			this.extensionRecommendationsManagementService.onDidChangeIgnoredRecommendations)(() => this.handleChangeRecommendations()));
-	
+			this.extensionRecommendationsManagementService.onDidChangeIgnoredRecommendations)(() => this._onDidChangeRecommendations.fire()));
+
 		this._register(this.extensionRecommendationsManagementService.onDidChangeGlobalIgnoredRecommendation(({ extensionId, isRecommended }) => {
 			if (!isRecommended) {
 				const reason = this.getAllRecommendationsWithReason()[extensionId];
@@ -133,21 +128,10 @@ export class ExtensionRecommendationsService extends Disposable implements IExte
 		}));
 
 		console.log('>> ExtensionRecommendationsService :: sleeping...');
-		await this.sleep(3000);
+		await new Promise(resolve => setTimeout(resolve, 3000));
 		console.log('>> ExtensionRecommendationsService :: process');
 
 		this.promptWorkspaceRecommendations();
-	}
-
-	private async handleChangeRecommendations(): Promise<void> {
-		console.log('>> ExtensionRecommendationsService :: handleChangeRecommendations');
-		this._onDidChangeRecommendations.fire();
-	}
-
-	private async sleep(milliseconds: number): Promise<void> {
-		return new Promise(resolve => {
-			setTimeout(resolve, milliseconds);
-		});
 	}
 
 	private isEnabled(): boolean {
