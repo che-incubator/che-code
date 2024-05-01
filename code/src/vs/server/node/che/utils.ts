a8ff0c08566343cf84f20839e3a26ee95d2abeb1
Roman Nikitenko <rnikiten@redhat.com>
2024-05-01 12:53:05 +0300

    add docs for the getResolvedPathEnvVar function
    Signed-off-by: Roman Nikitenko <rnikiten@redhat.com>
    
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED

--- code/src/vs/server/node/che/utils.ts
+++ code/src/vs/server/node/che/utils.ts
@@ -11,6 +11,22 @@
 
 import { delimiter } from 'vs/base/common/path';
 
+/**
+ * Merges the provided values. The first parameter is taken as the basis.
+ * Items from the second parameter are appended to the main value, duplicates are filtered out.
+ * For example:
+ * - the first parameter is: "/checode/checode-linux-libc/ubi9/bin/remote-cli:/usr/local/bin:/usr/bin" 
+ * - the second parameter is: "/go/bin:/usr/bin"
+ * - the function returns: "/checode/checode-linux-libc/ubi9/bin/remote-cli:/usr/local/bin:/usr/bin:/go/bin",
+ * - note: "/usr/bin" is filtered out to avoud duplicates in the returned value 
+ * 
+ * @param currentPath current value of the PATH env variable
+ * @param processEnvPath value of the process.env.PATH env variable 
+ * @returns
+ * - merged value for the given parameters 
+ * - currentPath if processEnvPath is not provided (undefined or empty string) 
+ */
+
 export function getResolvedPathEnvVar(currentPath: string, processEnvPath?: string): string {
     if (processEnvPath) {
         const currentPathArray: string[] = currentPath.split(delimiter);
