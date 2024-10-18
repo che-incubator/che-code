987d636cf7c385338d3bda6eb45887573c4c0bbc
vitaliy-guliy <vgulyy@redhat.com>
2024-10-18 14:15:45 +0000

    update
    Signed-off-by: vitaliy-guliy <vgulyy@redhat.com>

--- code/extensions/che-commands/src/default-extensions.ts
+++ code/extensions/che-commands/src/default-extensions.ts
@@ -11,27 +11,123 @@
 /* eslint-disable header/header */
 
 import * as vscode from 'vscode';
+import * as fs from 'fs-extra';
+import * as path from 'path';
 
 export class DefaultExtensions {
 
     async install(): Promise<void> {
-        const defaultExtensions = process.env.DEFAULT_EXTENSIONS;
-        if (!defaultExtensions) {
+        if (!process.env.DEFAULT_EXTENSIONS) {
             console.log('> Default extensions is not defined (DEFAULT_EXTENSIONS environment variable is unset).');
             return;
         }
 
-        const extensions: vscode.Uri[] = [];
+        try {
+            // get extesions from DEFAULT_EXTENSIONS environment cariable
+            let extensions = this.getDefaultExtensions();
 
-        const extensionList = defaultExtensions.split(';');
+            // filter the list, remove the extensions installed before
+            const installed = await this.readDotDefaultExtensionsFile();
+            extensions = extensions.filter(value => !installed.includes(value));
+
+            if (extensions.length) {
+                const result = await this.installExtensions(extensions);
+                if (result) {
+                    this.writeDotDefaultExtensionsFile(extensions);
+                }
+            }
+        } catch (error) {
+            console.log(`Failed to install default extensions. ${error}`);
+        }
+    }
+
+    getDefaultExtensions(): string[] {
+        const extensions: string[] = [];
+
+        console.log('--------------------------------------------');
+        console.log(`> Default etensions:`);
+
+        const extensionList = process.env.DEFAULT_EXTENSIONS!.split(';');
         for (const extension of extensionList) {
             if (extension.trim()) {
-                console.log(`> default extension [${extension.trim()}]`);
-                extensions.push(vscode.Uri.file(extension.trim()));
+                console.log(`    > [${extension.trim()}]`);
+                extensions.push(extension.trim());
             }
         }
+        console.log('--------------------------------------------');
 
-        vscode.window.withProgress({
+        return extensions;
+    }
+
+    async readDotDefaultExtensionsFile(): Promise<string[]> {
+        try {
+            const filePath = path.join(process.env.PROJECTS_ROOT!, '.default-extensions');
+            console.log(`> default extensions file path: ${filePath}`);
+
+            if (await fs.pathExists(filePath)) {
+                console.log(`> reading default extensions file...`);
+
+                const fileContent = await fs.readFile(filePath, 'utf8');
+                // console.log(fileContent);
+
+                const defaultExtensions: string[] = fileContent.split('\n');
+                for (const de of defaultExtensions) {
+                    console.log(`  > ${de}`);
+                }
+
+                return defaultExtensions;
+
+            } else {
+                console.log(`> default extensions file NOT found`);
+            }
+
+        } catch (error) {
+            console.log(`> error: ${error}`);
+        }
+
+        return [];
+    }
+
+    async writeDotDefaultExtensionsFile(defaultExtensions: string[]): Promise<void> {
+        console.log(`> write default extensions: ${defaultExtensions}`);
+
+        
+        try {
+            const filePath = path.join(process.env.PROJECTS_ROOT!, '.default-extensions');
+            console.log(`> default extensions file path: ${filePath}`);
+            
+            let fileContent: string = '';
+            if (await fs.pathExists(filePath)) {
+                fileContent = await fs.readFile(filePath, 'utf8');
+            }
+
+            let extensions: string[] = fileContent.split('\n').filter(value => (value));
+            for (const extension of defaultExtensions) {
+                if (!extensions.includes(extension)) {
+                    extensions.push(extension);
+                }
+            }
+
+            fileContent = extensions.join('\n');
+            console.log('------------------------------');
+            console.log(fileContent);
+            console.log('------------------------------');
+
+            await fs.writeFile(filePath, fileContent);
+
+        } catch (error) {
+            console.log(`Failure to write to .default-extensions file`);
+        }
+
+
+    }
+
+    async installExtensions(extensions: string[]): Promise<boolean> {
+        const toInstall: vscode.Uri[] = extensions.map(value => vscode.Uri.file(value));
+
+        let installed = false;
+
+        await vscode.window.withProgress({
             location: vscode.ProgressLocation.Window,
             cancellable: false,
             title: 'Installing default extensions'
@@ -39,8 +135,8 @@ export class DefaultExtensions {
             progress.report({ increment: 0 });
 
             try {
-                await vscode.commands.executeCommand('workbench.extensions.command.installFromVSIX', extensions);
-                progress.report({ increment: 100 });
+                await vscode.commands.executeCommand('workbench.extensions.command.installFromVSIX', toInstall);
+                installed = true;
             } catch (error) {
                 vscode.window.showInformationMessage(`Failed to install default extensions. ${error.message ? error.message : error}`);
             }
@@ -48,6 +144,7 @@ export class DefaultExtensions {
             progress.report({ increment: 100 });
         });
 
+        return installed;
     }
 
 }
