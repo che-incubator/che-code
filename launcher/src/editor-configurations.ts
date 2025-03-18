4a9614d06c6cdb65bdb875b688004e49582e3793
Vitaliy Gulyy <vgulyy@redhat.com>
2025-03-18 15:43:00 +0200

    feat: allow to configure product.json using configmap (#521)
    Signed-off-by: vitaliy-guliy <vgulyy@redhat.com>

--- launcher/src/editor-configurations.ts
+++ launcher/src/editor-configurations.ts
@@ -10,6 +10,8 @@
 
 import * as k8s from '@kubernetes/client-node';
 import * as fs from './fs-extra.js';
+import { ProductJSON } from './product-json.js';
+import { mergeFirstWithSecond, parseJSON } from './json-utils.js';
 
 const CONFIGMAP_NAME = 'vscode-editor-configurations';
 const REMOTE_SETTINGS_PATH = '/checode/remote/data/Machine/settings.json';
@@ -17,27 +19,35 @@ const REMOTE_SETTINGS_PATH = '/checode/remote/data/Machine/settings.json';
 const enum EditorConfigs {
   Settings = 'settings.json',
   Extensions = 'extensions.json',
+  Product = 'product.json',
 }
 
-export class EditorConfigurations {
-  private coreV1API: k8s.CoreV1Api;
+/**
+ * See following documentation for details
+ * https://eclipse.dev/che/docs/stable/administration-guide/editor-configurations-for-microsoft-visual-studio-code/
+ */
 
+export class EditorConfigurations {
   constructor(private readonly workspaceFilePath?: string) {}
 
   async configure(): Promise<void> {
-    console.log('# Checking if editor configurations are provided...');
+    console.log(`# Checking for editor configurations provided by '${CONFIGMAP_NAME}' Config Map...`);
+
+    if (!process.env.DEVWORKSPACE_NAMESPACE) {
+      console.log('  > process.env.DEVWORKSPACE_NAMESPACE is not set, skip this step');
+      return;
+    }
 
     try {
       const configmap = await this.getConfigmap();
       if (!configmap || !configmap.data) {
-        console.log('  > Editor configurations are not provided');
+        console.log(`  > Config Map ${CONFIGMAP_NAME} is not provided, skip this step`);
         return;
       }
 
-      const settingsPromise = this.configureSettings(configmap);
-      const extensionsPromise = this.configureExtensions(configmap);
-
-      await Promise.all([settingsPromise, extensionsPromise]);
+      await this.configureSettings(configmap);
+      await this.configureExtensions(configmap);
+      await this.configureProductJSON(configmap);
     } catch (error) {
       console.log(`  > Failed to apply editor configurations ${error}`);
     }
@@ -46,134 +56,120 @@ export class EditorConfigurations {
   private async configureSettings(configmap: k8s.V1ConfigMap): Promise<void> {
     const configmapContent = configmap.data![EditorConfigs.Settings];
     if (!configmapContent) {
-      console.log(`  > ${EditorConfigs.Settings} is not provided in the ${CONFIGMAP_NAME} configmap`);
-      return;
-    } else {
-      console.log(
-        `  > ${EditorConfigs.Settings} is provided in the ${CONFIGMAP_NAME} configmap, trying to apply it...`
-      );
-    }
-
-    const settingsFromConfigmap = parseJsonFrom(configmapContent);
-    if (!settingsFromConfigmap) {
-      console.log(
-        `    > Can not apply editor configurations: failed to parse ${EditorConfigs.Settings} data from the ${CONFIGMAP_NAME} configmap`
-      );
       return;
     }
 
-    let remoteSettingsJson;
-    if (await fs.fileExists(REMOTE_SETTINGS_PATH)) {
-      console.log(`    > File with settings is found: ${REMOTE_SETTINGS_PATH}`);
+    console.log('  > Configure editor settings...');
 
-      const remoteSettingsContent = await fs.readFile(REMOTE_SETTINGS_PATH);
-      remoteSettingsJson = parseJsonFrom(remoteSettingsContent);
-    } else {
-      console.log(`    > File with settings is not found, creating a new one: ${REMOTE_SETTINGS_PATH}`);
-      remoteSettingsJson = {};
+    try {
+      const settingsFromConfigmap = parseJSON(configmapContent, {
+        errorMessage: 'Configmap content is not valid.',
+      });
+
+      let remoteSettingsJson;
+      if (await fs.fileExists(REMOTE_SETTINGS_PATH)) {
+        console.log(`    > Found setings file: ${REMOTE_SETTINGS_PATH}`);
+        const remoteSettingsContent = await fs.readFile(REMOTE_SETTINGS_PATH);
+        remoteSettingsJson = parseJSON(remoteSettingsContent, {
+          errorMessage: 'Settings.json file is not valid.',
+        });
+      } else {
+        console.log(`    > Creating settings file: ${REMOTE_SETTINGS_PATH}`);
+        remoteSettingsJson = {};
+      }
+
+      const mergedSettings = { ...remoteSettingsJson, ...settingsFromConfigmap };
+      const json = JSON.stringify(mergedSettings, null, '\t');
+      await fs.writeFile(REMOTE_SETTINGS_PATH, json);
+
+      console.log('    > Editor settings have been configured.');
+    } catch (error) {
+      console.log('Failed to configure editor settings.', error);
     }
-
-    const mergedSettings = { ...remoteSettingsJson, ...settingsFromConfigmap };
-    const json = JSON.stringify(mergedSettings, null, '\t');
-    await fs.writeFile(REMOTE_SETTINGS_PATH, json);
-
-    console.log(`    > ${EditorConfigs.Settings} configs were applied successfully!`);
   }
 
   private async configureExtensions(configmap: k8s.V1ConfigMap): Promise<void> {
     const configmapContent = configmap.data![EditorConfigs.Extensions];
     if (!configmapContent) {
-      console.log(`  > ${EditorConfigs.Extensions} is not provided in the ${CONFIGMAP_NAME} configmap`);
-      return;
-    } else {
-      console.log(
-        `  > ${EditorConfigs.Extensions} is provided in the ${CONFIGMAP_NAME} configmap, trying to apply it...`
-      );
-    }
-
-    const extensionsFromConfigmap = parseJsonFrom(configmapContent);
-    if (!extensionsFromConfigmap) {
-      console.log(
-        `    > Can not apply editor configurations: failed to parse ${EditorConfigs.Extensions} data from the ${CONFIGMAP_NAME} configmap`
-      );
       return;
     }
 
-    if (this.workspaceFilePath && (await fs.fileExists(this.workspaceFilePath))) {
-      console.log(`    > File with configs is found: ${this.workspaceFilePath}`);
+    console.log('  > Configure workspace extensions...');
 
-      const workspaceFileContent = await fs.readFile(this.workspaceFilePath);
-      const workspaceConfigData = parseJsonFrom(workspaceFileContent);
-      if (!workspaceConfigData) {
-        console.log(
-          `    > Can not apply configurations for ${EditorConfigs.Extensions}: failed to parse data from the ${this.workspaceFilePath}`
-        );
+    try {
+      const extensionsFromConfigmap = parseJSON(configmapContent, {
+        errorMessage: 'Configmap content is not valid.',
+      });
+
+      if (!this.workspaceFilePath) {
+        console.log('    > Missing workspace file. Skip this step.');
         return;
       }
 
-      const extensionsSection = 'extensions';
-      if (!workspaceConfigData[extensionsSection]) {
-        console.log('    > Extensions section is absent in the workspace file, creating a new one...');
-        workspaceConfigData[extensionsSection] = {};
+      if (!(await fs.fileExists(this.workspaceFilePath))) {
+        console.log(`    > Unable to find workspace file: ${this.workspaceFilePath}. Skip this step.`);
+        return;
       }
 
-      workspaceConfigData[extensionsSection] = {
-        ...workspaceConfigData[extensionsSection],
+      console.log(`    > Found workspace file: ${this.workspaceFilePath}`);
+
+      const workspaceFileContent = await fs.readFile(this.workspaceFilePath);
+      const workspaceConfigData = parseJSON(workspaceFileContent, {
+        errorMessage: 'Workspace file is not valid.',
+      });
+
+      workspaceConfigData['extensions'] = {
+        ...(workspaceConfigData['extensions'] || {}),
         ...extensionsFromConfigmap,
       };
 
       const json = JSON.stringify(workspaceConfigData, null, '\t');
       await fs.writeFile(this.workspaceFilePath, json);
-      console.log(`    > ${EditorConfigs.Extensions} was applied successfully!`);
-    } else {
-      console.log(`    > Workspace Config File with is not found: ${this.workspaceFilePath}`);
-      // it's not possible to store extensions for the Remote scope,
-      // for sinle root mode extensions should come with a project, like: /project/.vscode/extensions.json - Folder scope
-      // so, we don't store extensions configs in another place if there is no workspace config file
-      //
+      console.log('    > Workspace extensions have been configured.');
+    } catch (error) {
+      console.log('Failed to configure workspace extensions.', error);
+    }
+  }
+
+  private async configureProductJSON(configmap: k8s.V1ConfigMap): Promise<void> {
+    const configmapContent = configmap.data![EditorConfigs.Product];
+    if (!configmapContent) {
       return;
+    }
+
+    console.log('  > Configure product.json ...');
+
+    try {
+      const productFromConfigmap = parseJSON(configmapContent, {
+        errorMessage: 'Configmap content is not valid.',
+      });
+
+      const product = new ProductJSON();
+      await product.load();
+
+      mergeFirstWithSecond(productFromConfigmap, product.get());
+
+      await product.save();
+
+      console.log('    > product.json have been configured.');
+    } catch (error) {
+      console.log(`Failed to configure ${EditorConfigs.Product}.`, error);
     }
   }
 
   private async getConfigmap(): Promise<k8s.V1ConfigMap | undefined> {
-    const coreV1API = this.getCoreApi();
-    const namespace = process.env.DEVWORKSPACE_NAMESPACE;
-    if (!namespace) {
-      console.log(
-        '  > Error: Can not get Configmap with editor configurations - DEVWORKSPACE_NAMESPACE env variable is not defined'
-      );
-      return undefined;
-    }
+    const k8sConfig = new k8s.KubeConfig();
+    k8sConfig.loadFromCluster();
+    const coreV1API = k8sConfig.makeApiClient(k8s.CoreV1Api);
 
     try {
-      const { body } = await coreV1API.readNamespacedConfigMap(CONFIGMAP_NAME, namespace);
+      const { body } = await coreV1API.readNamespacedConfigMap(CONFIGMAP_NAME, process.env.DEVWORKSPACE_NAMESPACE!);
       return body;
     } catch (error) {
       console.log(
-        `#   > Warning: Can not get Configmap with editor configurations: ${error.message}, status code: ${error?.response?.statusCode}`
+        `  > Warning: Can not get Configmap with editor configurations: ${error.message}, status code: ${error?.response?.statusCode}`
       );
       return undefined;
     }
-  }
-
-  getCoreApi(): k8s.CoreV1Api {
-    if (!this.coreV1API) {
-      const k8sConfig = new k8s.KubeConfig();
-      k8sConfig.loadFromCluster();
-      this.coreV1API = k8sConfig.makeApiClient(k8s.CoreV1Api);
-    }
-    return this.coreV1API;
-  }
-}
-
-export function parseJsonFrom(content: string): any {
-  if (!content) {
-    return undefined;
-  }
-
-  try {
-    return JSON.parse(content);
-  } catch (parseError) {
-    console.error(`  > Error parsing JSON: ${parseError}`);
   }
 }
