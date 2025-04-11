cf4c5b6f34f0ffb0cf92ca4afd15e4cba9fa0ab6
Roman Nikitenko <rnikiten@redhat.com>
2025-04-11 13:30:09 +0000

    chore: Add rules for automatic rebasing
    Signed-off-by: Roman Nikitenko <rnikiten@redhat.com>

--- rebase.sh
+++ rebase.sh
@@ -453,6 +453,8 @@ resolve_conflicts() {
       apply_changes "$conflictingFile"
     elif [[ "$conflictingFile" == "code/src/vs/workbench/contrib/extensions/browser/extensions.contribution.ts" ]]; then
       apply_changes "$conflictingFile"
+    elif [[ "$conflictingFile" == "code/src/vs/platform/utilityProcess/electron-main/utilityProcess.ts" ]]; then
+      apply_changes "$conflictingFile"
     elif [[ "$conflictingFile" == "code/src/vs/platform/extensionManagement/node/extensionManagementService.ts" ]]; then
       apply_changes "$conflictingFile"
     else
