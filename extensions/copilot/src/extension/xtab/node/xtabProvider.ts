e233524d8ac0d2b5bbbb4d7db2250d425a812de9
Ulugbek Abdullaev <ulugbekna@gmail.com>
2025-07-04 14:30:49 +0200

    nes: refactor computing predicted output (#111)

--- extensions/copilot/src/extension/xtab/node/xtabProvider.ts
+++ extensions/copilot/src/extension/xtab/node/xtabProvider.ts
@@ -287,14 +287,7 @@ export class XtabProvider extends ChainedStatelessNextEditProvider {
 
 		const userPrompt = getUserPrompt(request, taggedCurrentFileContent, areaAroundCodeToEdit, langCtx, computeTokens, promptOptions);
 
-		const prediction = this.configService.getConfig(ConfigKey.Internal.InlineEditsXtabProviderUsePrediction)
-			? {
-				type: 'content',
-				content: (
-					XtabProvider.getPredictedOutput(editWindowLines, promptOptions.promptingStrategy)
-				)
-			} as const
-			: undefined;
+		const prediction = this.getPredictedOutput(editWindowLines, promptOptions.promptingStrategy);
 
 		const messages = [
 			{
@@ -831,7 +824,16 @@ export class XtabProvider extends ChainedStatelessNextEditProvider {
 		return this.instaService.createInstance(ProxyXtabEndpoint, modelName);
 	}
 
-	private static getPredictedOutput(editWindowLines: readonly string[], promptingStrategy: xtabPromptOptions.PromptingStrategy | undefined): string {
+	private getPredictedOutput(editWindowLines: string[], promptingStrategy: xtabPromptOptions.PromptingStrategy | undefined): Prediction | undefined {
+		return this.configService.getConfig(ConfigKey.Internal.InlineEditsXtabProviderUsePrediction)
+			? {
+				type: 'content',
+				content: XtabProvider.getPredictionContents(editWindowLines, promptingStrategy)
+			}
+			: undefined;
+	}
+
+	private static getPredictionContents(editWindowLines: readonly string[], promptingStrategy: xtabPromptOptions.PromptingStrategy | undefined): string {
 		if (promptingStrategy === xtabPromptOptions.PromptingStrategy.UnifiedModel) {
 			return ['<EDIT>', ...editWindowLines, '</EDIT>'].join('\n');
 		} else if (promptingStrategy === xtabPromptOptions.PromptingStrategy.Xtab275) {
