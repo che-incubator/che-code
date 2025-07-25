6d4c80774fdcc87b4f3af9149cf81e8b71f21faa
Roman Nikitenko <rnikiten@redhat.com>
2025-07-25 14:02:58 +0000

    chore: Provide rule for automatic rebasing against upstream
    Signed-off-by: Roman Nikitenko <rnikiten@redhat.com>

--- rebase.sh
+++ rebase.sh
@@ -482,6 +482,8 @@ resolve_conflicts() {
       apply_changes "$conflictingFile"
     elif [[ "$conflictingFile" == "code/src/vs/platform/extensionManagement/node/extensionManagementService.ts" ]]; then
       apply_changes "$conflictingFile"
+    elif [[ "$conflictingFile" == "code/src/vs/platform/extensionManagement/common/abstractExtensionManagementService.ts" ]]; then
+      apply_changes "$conflictingFile"
     elif [[ "$conflictingFile" == "code/src/vs/workbench/contrib/chat/browser/actions/chatActions.ts" ]]; then
       apply_multi_line_replace "$conflictingFile"
     else
