c5e4fdf11e62cc01fadc83172b2ee19a25d7a44d
Matt Bierner <matb@microsoft.com>
2025-08-12 11:42:27 -0700

    Clarify that simple browser only supports http and https uris (#571)
    Fixes microsoft/vscode#261082

--- extensions/copilot/src/extension/tools/node/simpleBrowserTool.tsx
+++ extensions/copilot/src/extension/tools/node/simpleBrowserTool.tsx
@@ -7,6 +7,7 @@ import * as l10n from '@vscode/l10n';
 import type * as vscode from 'vscode';
 import { IRunCommandExecutionService } from '../../../platform/commands/common/runCommandExecutionService';
 import { ResourceSet } from '../../../util/vs/base/common/map';
+import { Schemas } from '../../../util/vs/base/common/network';
 import { URI } from '../../../util/vs/base/common/uri';
 import { LanguageModelTextPart, LanguageModelToolResult, MarkdownString } from '../../../vscodeTypes';
 import { IBuildPromptContext } from '../../prompt/common/intents';
@@ -27,7 +28,8 @@ export class SimpleBrowserTool implements ICopilotTool<ISimpleBrowserParams> {
 	) { }
 
 	async invoke(options: vscode.LanguageModelToolInvocationOptions<ISimpleBrowserParams>, token: vscode.CancellationToken) {
-		this._alreadyApprovedDomains.add(URI.parse(options.input.url));
+		const uri = URI.parse(options.input.url);
+		this._alreadyApprovedDomains.add(uri);
 		this.commandService.executeCommand('simpleBrowser.show', options.input.url);
 		return new LanguageModelToolResult([
 			new LanguageModelTextPart(
@@ -41,7 +43,12 @@ export class SimpleBrowserTool implements ICopilotTool<ISimpleBrowserParams> {
 	}
 
 	prepareInvocation(options: vscode.LanguageModelToolInvocationPrepareOptions<ISimpleBrowserParams>, token: vscode.CancellationToken): vscode.ProviderResult<vscode.PreparedToolInvocation> {
-		const urlsNeedingConfirmation = !this._alreadyApprovedDomains.has(URI.parse(options.input.url));
+		const uri = URI.parse(options.input.url);
+		if (uri.scheme !== Schemas.http && uri.scheme !== Schemas.https) {
+			throw new Error(l10n.t('Invalid URL scheme. Only HTTP and HTTPS are supported.'));
+		}
+
+		const urlsNeedingConfirmation = !this._alreadyApprovedDomains.has(uri);
 		let confirmationMessages: vscode.LanguageModelToolConfirmationMessages | undefined;
 		if (urlsNeedingConfirmation) {
 			confirmationMessages = { title: l10n.t`Open untrusted web page?`, message: new MarkdownString(l10n.t`${options.input.url}`) };
