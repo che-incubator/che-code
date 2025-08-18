dc659c47cc24509c2ea8c9537147ed0427b36f62
Connor Peet <connor@peet.io>
2025-08-18 15:24:34 -0700

    edits: fix add file operation failing with apply_patch (#654)

--- extensions/copilot/src/extension/tools/node/applyPatchTool.tsx
+++ extensions/copilot/src/extension/tools/node/applyPatchTool.tsx
@@ -45,9 +45,9 @@ import { IToolsService } from '../common/toolsService';
 import { PATCH_PREFIX, PATCH_SUFFIX } from './applyPatch/parseApplyPatch';
 import { ActionType, Commit, DiffError, FileChange, identify_files_needed, InvalidContextError, InvalidPatchFormatError, processPatch } from './applyPatch/parser';
 import { EditFileResult, IEditedFile } from './editFileToolResult';
+import { createEditConfirmation } from './editFileToolUtils';
 import { sendEditNotebookTelemetry } from './editNotebookTool';
 import { assertFileOkForTool, resolveToolInputPath } from './toolUtils';
-import { createEditConfirmation } from './editFileToolUtils';
 
 export const applyPatchWithNotebookSupportDescription: vscode.LanguageModelToolInformation = {
 	name: ToolName.ApplyPatch,
@@ -335,12 +335,15 @@ export class ApplyPatchTool implements ICopilotTool<IApplyPatchToolParams> {
 				const existingDiagnostics = this.languageDiagnosticsService.getDiagnostics(uri);
 
 				// Initialize edit survival tracking for text documents
-				const document = notebookUri ?
-					await this.workspaceService.openNotebookDocumentAndSnapshot(notebookUri, this.alternativeNotebookContent.getFormat(this._promptContext?.request?.model)) :
-					await this.workspaceService.openTextDocumentAndSnapshot(uri);
-				if (document instanceof TextDocumentSnapshot) {
-					const tracker = this._editSurvivalTrackerService.initialize(document.document);
-					editSurvivalTrackers.set(uri, tracker);
+				const existsOnDisk = await this.fileSystemService.stat(uri).then(() => true, () => false);
+				if (existsOnDisk) {
+					const document = notebookUri ?
+						await this.workspaceService.openNotebookDocumentAndSnapshot(notebookUri, this.alternativeNotebookContent.getFormat(this._promptContext?.request?.model)) :
+						await this.workspaceService.openTextDocumentAndSnapshot(uri);
+					if (document instanceof TextDocumentSnapshot) {
+						const tracker = this._editSurvivalTrackerService.initialize(document.document);
+						editSurvivalTrackers.set(uri, tracker);
+					}
 				}
 
 				if (notebookUri) {
