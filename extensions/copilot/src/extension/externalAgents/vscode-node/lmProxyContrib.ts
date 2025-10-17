09c118dcee16a2b91a03b612d83e7ecc55933ab9
Rob Lourens <roblourens@gmail.com>
2025-10-16 21:12:07 -0700

    Only register proxy when authed (#1388)
    * Only register proxy when authed
    
    * Register it

--- extensions/copilot/src/extension/externalAgents/vscode-node/lmProxyContrib.ts
+++ extensions/copilot/src/extension/externalAgents/vscode-node/lmProxyContrib.ts
@@ -4,19 +4,35 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as vscode from 'vscode';
-import { Disposable } from '../../../util/vs/base/common/lifecycle';
+import { IAuthenticationService } from '../../../platform/authentication/common/authentication';
+import { Disposable, MutableDisposable } from '../../../util/vs/base/common/lifecycle';
 import { IInstantiationService } from '../../../util/vs/platform/instantiation/common/instantiation';
 import { IExtensionContribution } from '../../common/contributions';
 import { LanguageModelProxyProvider } from '../node/modelProxyProvider';
+import { Event } from '../../../util/vs/base/common/event';
 
 export class LanguageModelProxyContrib extends Disposable implements IExtensionContribution {
 	readonly id = 'LanguageModelProxy';
 
 	constructor(
 		@IInstantiationService instantiationService: IInstantiationService,
+		@IAuthenticationService authenticationService: IAuthenticationService
 	) {
 		super();
 
-		this._register(vscode.lm.registerLanguageModelProxyProvider(instantiationService.createInstance(LanguageModelProxyProvider)));
+		const providerDisposable = this._register(new MutableDisposable<vscode.Disposable>());
+		const updateRegistration = () => {
+			const token = authenticationService.copilotToken;
+			// if (token?.sku === '') { // ??
+			// }
+
+			if (!providerDisposable.value && token) {
+				providerDisposable.value = vscode.lm.registerLanguageModelProxyProvider(instantiationService.createInstance(LanguageModelProxyProvider));
+			} else if (providerDisposable.value && !token) {
+				providerDisposable.clear();
+			}
+		};
+
+		this._register(Event.runAndSubscribe(authenticationService.onDidAuthenticationChange, updateRegistration));
 	}
 }
