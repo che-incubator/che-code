7945d35c06a9aa215c84f3b95e2e56e30894511a
Christof Marti <chrmarti@microsoft.com>
2025-11-11 16:19:43 +0100

    Scrub error detail (#1920)

--- extensions/copilot/src/extension/prompt/node/chatMLFetcher.ts
+++ extensions/copilot/src/extension/prompt/node/chatMLFetcher.ts
@@ -1018,6 +1018,7 @@ export class ChatMLFetcherImpl extends AbstractChatMLFetcher {
 	}
 
 	private scrubErrorDetail(errorDetail: string) {
+		errorDetail = errorDetail.replaceAll(/(logged in as )([^\s]+)/ig, '$1<login>');
 		const username = this._authenticationService.copilotToken?.username;
 		if (!username) {
 			return errorDetail;
