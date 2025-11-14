24eb86e92d8210dfa3bb0db12b9f719665a8f085
Johannes Rieken <johannes.rieken@gmail.com>
2025-11-14 15:39:26 +0100

    make sure to set metadata required for telemetry sending (#1999)
    https://github.com/microsoft/vscode/issues/277422

--- extensions/copilot/src/extension/inlineChat/node/inlineChatIntent.ts
+++ extensions/copilot/src/extension/inlineChat/node/inlineChatIntent.ts
@@ -6,9 +6,11 @@
 import type * as vscode from 'vscode';
 import { IAuthenticationService } from '../../../platform/authentication/common/authentication';
 import { ChatFetchResponseType, ChatLocation, getErrorDetailsFromChatFetchError } from '../../../platform/chat/common/commonTypes';
+import { IEditSurvivalTrackerService } from '../../../platform/editSurvivalTracking/common/editSurvivalTrackerService';
 import { IEndpointProvider } from '../../../platform/endpoint/common/endpointProvider';
 import { IIgnoreService } from '../../../platform/ignore/common/ignoreService';
 import { ILogService } from '../../../platform/log/common/logService';
+import { ChatResponseStreamImpl } from '../../../util/common/chatResponseStreamImpl';
 import { isNonEmptyArray } from '../../../util/vs/base/common/arrays';
 import { CancellationToken } from '../../../util/vs/base/common/cancellation';
 import { toErrorMessage } from '../../../util/vs/base/common/errorMessage';
@@ -16,7 +18,7 @@ import { Event } from '../../../util/vs/base/common/event';
 import { assertType } from '../../../util/vs/base/common/types';
 import { localize } from '../../../util/vs/nls';
 import { IInstantiationService } from '../../../util/vs/platform/instantiation/common/instantiation';
-import { ChatRequestEditorData } from '../../../vscodeTypes';
+import { ChatRequestEditorData, ChatResponseTextEditPart } from '../../../vscodeTypes';
 import { Intent } from '../../common/constants';
 import { getAgentTools } from '../../intents/node/agentIntent';
 import { ChatVariablesCollection } from '../../prompt/common/chatVariablesCollection';
@@ -24,6 +26,7 @@ import { Conversation } from '../../prompt/common/conversation';
 import { IToolCall } from '../../prompt/common/intents';
 import { ToolCallRound } from '../../prompt/common/toolCallRound';
 import { ChatTelemetryBuilder } from '../../prompt/node/chatParticipantTelemetry';
+import { IntentInvocationMetadata } from '../../prompt/node/conversation';
 import { IDocumentContext } from '../../prompt/node/documentContext';
 import { IIntent } from '../../prompt/node/intents';
 import { PromptRenderer } from '../../prompts/node/base/promptRenderer';
@@ -32,7 +35,7 @@ import { ToolName } from '../../tools/common/toolNames';
 import { normalizeToolSchema } from '../../tools/common/toolSchemaNormalizer';
 import { CopilotToolMode } from '../../tools/common/toolsRegistry';
 import { isToolValidationError, isValidatedToolInput, IToolsService } from '../../tools/common/toolsService';
-import { InteractionOutcomeComputer } from './promptCraftingTypes';
+import { CopilotInteractiveEditorResponse, InteractionOutcomeComputer } from './promptCraftingTypes';
 
 
 const INLINE_CHAT_EXIT_TOOL_NAME = 'inline_chat_exit';
@@ -61,11 +64,13 @@ export class InlineChatIntent implements IIntent {
 		@ILogService private readonly _logService: ILogService,
 		@IToolsService private readonly _toolsService: IToolsService,
 		@IIgnoreService private readonly _ignoreService: IIgnoreService,
+		@IEditSurvivalTrackerService private readonly _editSurvivalTrackerService: IEditSurvivalTrackerService,
 	) { }
 
 	async handleRequest(conversation: Conversation, request: vscode.ChatRequest, stream: vscode.ChatResponseStream, token: CancellationToken, documentContext: IDocumentContext | undefined, agentName: string, _location: ChatLocation, chatTelemetry: ChatTelemetryBuilder, onPaused: Event<boolean>): Promise<vscode.ChatResult> {
 
 		assertType(request.location2 instanceof ChatRequestEditorData);
+		assertType(documentContext);
 
 		if (await this._ignoreService.isCopilotIgnored(request.location2.document.uri, token)) {
 			return {
@@ -99,8 +104,17 @@ export class InlineChatIntent implements IIntent {
 
 		const telemetry = chatTelemetry.makeRequest(this, ChatLocation.Editor, conversation, renderResult.messages, renderResult.tokenCount, renderResult.references, endpoint, [], inlineChatTools.length);
 		const outcomeComputer = new InteractionOutcomeComputer(request.location2.document.uri);
+		const editSurvivalTracker = this._editSurvivalTrackerService.initialize(request.location2.document);
+
+		stream = ChatResponseStreamImpl.spy(stream, part => {
+			if (part instanceof ChatResponseTextEditPart) {
+				editSurvivalTracker.collectAIEdits(part.edits);
+				telemetry.markEmittedEdits(part.uri, part.edits);
+			}
+		});
 
 		stream = outcomeComputer.spyOnStream(stream);
+
 		const toolCalls: IToolCall[] = [];
 		let toolError: unknown | undefined;
 
@@ -110,6 +124,8 @@ export class InlineChatIntent implements IIntent {
 			userInitiatedRequest: true,
 			location: ChatLocation.Editor,
 			finishedCb: async (_text, _index, delta) => {
+
+				telemetry.markReceivedToken();
 
 				let doneAfterToolCalls = false;
 
@@ -224,6 +240,21 @@ export class InlineChatIntent implements IIntent {
 			// when no tools were called, invoke the exit tool manually
 			await this._toolsService.invokeTool(INLINE_CHAT_EXIT_TOOL_NAME, { toolInvocationToken: request.toolInvocationToken, input: undefined }, token);
 		}
+
+		// store metadata for telemetry sending
+		const turn = conversation.getLatestTurn();
+		turn.setMetadata(outcomeComputer.interactionOutcome);
+		turn.setMetadata(new CopilotInteractiveEditorResponse(
+			'ok', outcomeComputer.store,
+			{ ...documentContext, query: request.prompt, intent: this },
+			telemetry.telemetryMessageId, telemetry, editSurvivalTracker
+		));
+		turn.setMetadata(new IntentInvocationMetadata({ // UGLY fake intent invocation
+			location: ChatLocation.Editor,
+			intent: this,
+			endpoint: endpoint,
+			buildPrompt: () => { throw new Error(); },
+		}));
 
 		return {};
 
