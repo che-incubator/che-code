33754cda524cb52101e2d17b091ccc161396f304
Matt Bierner <12821956+mjbvz@users.noreply.github.com>
2025-12-05 09:54:02 -0800

    Run esbuild script directly as typescript (#2445)

--- extensions/copilot/.esbuild.ts
+++ extensions/copilot/.esbuild.ts
@@ -10,7 +10,7 @@ import { copyFile, mkdir } from 'fs/promises';
 import { glob } from 'glob';
 import * as path from 'path';
 
-const REPO_ROOT = path.join(__dirname);
+const REPO_ROOT = import.meta.dirname;
 const isWatch = process.argv.includes('--watch');
 const isDev = process.argv.includes('--dev');
 const isPreRelease = process.argv.includes('--prerelease');
@@ -251,8 +251,8 @@ const nodeSimulationWorkbenchUIBuildOptions = {
 
 async function typeScriptServerPluginPackageJsonInstall(): Promise<void> {
 	await mkdir('./node_modules/@vscode/copilot-typescript-server-plugin', { recursive: true });
-	const source = path.join(__dirname, './src/extension/typescriptContext/serverPlugin/package.json');
-	const destination = path.join(__dirname, './node_modules/@vscode/copilot-typescript-server-plugin/package.json');
+	const source = path.join(import.meta.dirname, './src/extension/typescriptContext/serverPlugin/package.json');
+	const destination = path.join(import.meta.dirname, './node_modules/@vscode/copilot-typescript-server-plugin/package.json');
 	try {
 		await copyFile(source, destination);
 	} catch (error) {
@@ -369,7 +369,7 @@ async function main() {
 }
 
 function applyPackageJsonPatch(isPreRelease: boolean) {
-	const packagejsonPath = path.join(__dirname, './package.json');
+	const packagejsonPath = path.join(import.meta.dirname, './package.json');
 	const json = JSON.parse(fs.readFileSync(packagejsonPath).toString());
 
 	const newProps: any = {
