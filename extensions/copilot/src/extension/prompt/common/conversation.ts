88bde5d9b2dcb335455cd6cdab4b816aca7b876b
Rob Lourens <roblourens@gmail.com>
2025-12-20 18:22:21 -0800

    Don't recreate ToolCallRounds when missing (#2655)
    So that they can be mutated and retain that state
    Fix #272987

--- extensions/copilot/src/extension/prompt/common/conversation.ts
+++ extensions/copilot/src/extension/prompt/common/conversation.ts
@@ -133,13 +133,23 @@ export class Turn {
 		return metadata?.renderedUserMessage;
 	}
 
+	// TODO@roblourens Tracking result data in "agent as chat participant" is difficult and will be replaced in the future.
+	// This is likely a Turn from Ask mode that does not have tool call rounds.
+	// Use consistent instances so we can save state on them.
+	private _filledInMissingRounds: IToolCallRound[] | undefined;
+
 	get rounds(): readonly IToolCallRound[] {
 		const metadata = this.resultMetadata;
 		const rounds = metadata?.toolCallRounds;
 		if (!rounds || rounds.length === 0) {
+			if (this._filledInMissingRounds?.length) {
+				return this._filledInMissingRounds;
+			}
+
 			// Should always have at least one round
 			const response = this.responseMessage?.message ?? '';
-			return [new ToolCallRound(response, [], undefined, this.id)];
+			this._filledInMissingRounds = [new ToolCallRound(response, [], undefined, this.id)];
+			return this._filledInMissingRounds;
 		}
 
 		return rounds;
