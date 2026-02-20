0cbf396d35d441b8d9a4ba6b88260054b1249c12
Ulugbek Abdullaev <ulugbekna@gmail.com>
2026-02-20 13:45:32 +0500

    nes: ghost: forward handleListEndOfLifetime to ghost provider (#3854)

--- extensions/copilot/src/extension/inlineEdits/vscode-node/jointInlineCompletionProvider.ts
+++ extensions/copilot/src/extension/inlineEdits/vscode-node/jointInlineCompletionProvider.ts
@@ -784,7 +784,7 @@ class JointCompletionsProvider extends Disposable implements vscode.InlineComple
 	public handleListEndOfLifetime?(list: SingularCompletionList, reason: vscode.InlineCompletionsDisposeReason): void {
 		switch (list.source) {
 			case 'completions':
-				softAssert(this._completionsProvider?.handleListEndOfLifetime === undefined, 'CompletionsProvider does not implement handleListEndOfLifetime');
+				this._completionsProvider?.handleListEndOfLifetime?.(list, reason);
 				break;
 			case 'inlineEdits':
 				this._inlineEditProvider?.handleListEndOfLifetime?.(list, reason);
